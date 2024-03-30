357b7b1341be665a5f89eea8b01ef427a3c4fd6b
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-03-30 18:40:07 +0530

    able to add custom labels

--- routes/gmail.routes.js
+++ routes/gmail.routes.js
@@ -8,7 +8,7 @@ GmailRouter.get("/userInfo/:userId",async(req,res)=>{
     try {
         let {userId} = req.params
         let access_token = await redisConnection.get(userId)
-
+        
         let response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/${userId}/profile`,{
             headers:{
                 "Content-Type" : "application/json",
@@ -23,7 +23,26 @@ GmailRouter.get("/userInfo/:userId",async(req,res)=>{
     }
 })
 
+GmailRouter.post("/createLabel/:userId",async(req,res)=>{
+    try {
+        let {userId} = req.params
+        let access_token = await redisConnection.get(userId)
+        
+        let labelContent = req.body
 
+        let response = await axios.post(`https://gmail.googleapis.com/gmail/v1/users/${userId}/labels`,labelContent,{
+            headers:{
+                "Content-Type" : "application/json",
+                "Authorization" :`Bearer ${access_token}`
+            }
+        })
+
+        res.status(200).json(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({Error:"Error while creating new label"})
+    }
+})
 
 
 
