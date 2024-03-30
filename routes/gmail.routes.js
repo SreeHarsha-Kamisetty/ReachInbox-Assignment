10991b9cd4a4084d0828a36d9217dd8e45472d43
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-03-30 19:02:26 +0530

    get list of user emails, read email by messageid

--- routes/gmail.routes.js
+++ routes/gmail.routes.js
@@ -44,7 +44,44 @@ GmailRouter.post("/createLabel/:userId",async(req,res)=>{
     }
 })
 
+GmailRouter.get("/list/:userId",async(req,res)=>{
+    try {
+        let {userId} = req.params
+        let access_token = await redisConnection.get(userId)
+        
+        let response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/${userId}/messages`,{
+            headers:{
+                "Content-Type" : "application/json",
+                "Authorization" :`Bearer ${access_token}`
+            }
+        })
+       
+        res.status(200).json(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({Error:"Error while getting email list"})
+    }
+})
 
+GmailRouter.get("/read/:userId/messages/:id",async(req,res)=>{
+    try {
+        let {userId,id} = req.params
+        
+        let access_token = await redisConnection.get(userId)
+        
+        let response = await axios.get(`https://gmail.googleapis.com/gmail/v1/users/${userId}/messages/${id}`,{
+            headers:{
+                "Content-Type" : "application/json",
+                "Authorization" :`Bearer ${access_token}`
+            }
+        })
+       
+        res.status(200).json(response.data)
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({Error:"Error while getting email list"})
+    }
+})
 
 module.exports={
     GmailRouter
